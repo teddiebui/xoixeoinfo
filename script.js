f87730a5acc696bafd0cf8a9faa533667aba6e64
sapphiral <56861110+sapphiral@users.noreply.github.com>
2024-04-21 11:32:08 +0700

    commit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
 $(document).ready(function() {
-    
+    $(".hover").hover(
+        function () { // Mouse enter
+            $(this).addClass('hovered');
+        },
+        function () { // Mouse leave
+            $(this).removeClass('hovered');
+        }
+    )
 })
